fcc6cfae5c6896a750ff2b6062e8f51e57f007b9
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-07-17 00:58:35 -0500

    VR demo explaining the usage of Oculus Utilities Kit

---
+++ unity-assignments/Oculus-Utilities-Demo/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class cameraMovement : MonoBehaviour {
+
+	public GameObject displayTextGameObject;
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = displayTextGameObject.GetComponent<Text> ();
+		text.text = "";
+
+		OVRTouchpad.Create();
+		OVRTouchpad.TouchHandler += HandleTouchHandler;
+	}
+
+	void HandleTouchHandler (object sender, System.EventArgs e)
+	{
+		OVRTouchpad.TouchArgs touchArgs = (OVRTouchpad.TouchArgs) e;
+
+		if (touchArgs.TouchType == OVRTouchpad.TouchEvent.SingleTap) {
+			text.text = "Single tap !";
+		}
+		if (touchArgs.TouchType == OVRTouchpad.TouchEvent.Up) {
+			text.text = "Going Up !";
+		}
+		if (touchArgs.TouchType == OVRTouchpad.TouchEvent.Down) {
+			text.text = "Heading Down !";
+		}
+		if (touchArgs.TouchType == OVRTouchpad.TouchEvent.Right) {
+			text.text = "Going right !";
+		}
+		if (touchArgs.TouchType == OVRTouchpad.TouchEvent.Left) {
+			text.text = "Going Left !";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			Vector3 movedPosition = new Vector3 (Input.GetAxis ("Mouse X"), 0, Input.GetAxis ("Mouse Y"));
+			movedPosition = new Vector3(-movedPosition.z, 0, -movedPosition.x);
+
+			transform.position += movedPosition;
+		}
+	}
+}
